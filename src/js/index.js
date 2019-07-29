0b7f27bafbeea228a2557a9a9a76e98032af51ac
Oli Lucas <Oli.Lucas@thinkorchard.com>
2019-07-29 16:52:18 +0100

    profile images added

--- src/js/index.js
+++ src/js/index.js
@@ -2302,7 +2302,7 @@ import '../styles/main.styl';
 	    var _this2 = this;
 	    this.prevEl = this.currentEl;this.currentEl = el;
 		this.prevEl && (this.prevEl.style["z-index"] = 0);
-		console.log("show element");
+
 	    // return immediately on edges
 	    if (el.delta < 0.2) {
 	      return;
@@ -2313,7 +2313,7 @@ import '../styles/main.styl';
 			contentEl = el.querySelector(".particle__content"),
 			modalOverlay = document.querySelector("#modal-bg"),
 	        tween = new mojs.Tween();
-		console.log(modalOverlay);
+
 	    this.isOpen = true;el.style["z-index"] = 20;this.iscroll.enabled = false;
 		this.showInnerCircle(el.x + 75, el.y + 75);this.iscroll.scrollTo(-x, -y, 500 * this.S);
 		
@@ -2329,9 +2329,7 @@ import '../styles/main.styl';
 	    var scaleDownTween = new mojs.Timeline({
 	      duration: 300 * this.S, easing: "expo.out",
 	      onUpdate: function (p) {
-	        //mojs.h.setPrefixedStyle(innerEl, "transform", "scale(" + (1 - 0.25 * p) + ") translateZ(0)");
-			innerEl.style.opacity = 1 - 0.25 * p;
-			//mojs.h.setPrefixedStyle(innerEl, "transition", "scale(all .2s ease-in)");
+	       innerEl.style.opacity = 1 - 0.25 * p;
 	      }
 	    });
 
@@ -2351,17 +2349,7 @@ import '../styles/main.styl';
 	        var scaleSize = 19 * mojs.easing.cubic["in"](p);
 	        scaleSize = Math.max(0.75, scaleSize);
 			var scale = "scale(" + scaleSize + ") translateZ(0)";
-			// modalOverlay = modalOverlay.style.opacity = 0.75 + 0.25;
-			//var modalOverlay = document.querySelector("#modal-bg");
-			//this.modalOverlay = document.querySelector("#modal-bg");
-		   // console.log(modalOverlay);
-			var imageURL = 	innerEl.style.backgroundImage;
 
-			//console.log(imageURL);
-
-			//mojs.h.setPrefixedStyle(innerEl, "transform", scale);
-			//mojs.h.setPrefixedStyle(innerEl, "background-image", "none");
-			//mojs.h.setPrefixedStyle(innerEl, "transition", "background 100ms linear");
 	        innerEl.style.opacity = 0.75 + 0.25 * mojs.easing.cubic.out(p);
 	      },
 	      onStart: function () {
@@ -2405,8 +2393,6 @@ import '../styles/main.styl';
 		modalOverlay.style["z-index"] = 0;
 		modalOverlay.style["opacity"] = 0;	
 
-		//var imageURL = 	innerEl.style.backgroundImage;
-		//console.log(imageURL);
 	    var scaleDownTimeline = new mojs.Timeline({
 	      duration: 500 * this.S,
 	      onUpdate: function (p) {
@@ -2415,8 +2401,6 @@ import '../styles/main.styl';
 	            scaleSize = 0.75 + 18 * npe,
 	            scale = "scale(" + scaleSize + ") translateZ(0)";
 
-			//mojs.h.setPrefixedStyle(innerEl, "transform", scale);
-			//mojs.h.setPrefixedStyle(innerEl, "background-image", scale);
 	        mojs.h.setPrefixedStyle(_this.content, "transform", "scale(" + npe + ") translateZ(0)");
 	      }
 	    });
@@ -2432,7 +2416,6 @@ import '../styles/main.styl';
 	      onUpdate: function (p) {
 	        var scaleSize = 0.75 + 0.25 * mojs.easing.elastic.out(p),
 	            scale = "scale(" + scaleSize + ") translateZ(0)";
-	       // mojs.h.setPrefixedStyle(innerEl, "transform", scale);
 	      },
 	      onComplete: function () {
 	        if (_this.isOpen) {
@@ -2542,7 +2525,7 @@ import '../styles/main.styl';
 	        scene = el.querySelector(".shape"),
 	        shadow = el.querySelector("#js-shadow"),
 	        shadowWrap = el.querySelector("#js-shadow-wrap");
-			console.log("Show inner plastic");
+		
 	    this.runDust();
 
 	    var mp = new mojs.MotionPath({
@@ -2771,15 +2754,15 @@ import '../styles/main.styl';
 	    this.loadCnt === this.maxLoadCnt && this.start();
 	  },
 	  loadAssets: function () {
-	    this.openSound = new Howl({ urls: ["sounds/open-bubble-2." + this.ext], onload: this.updateProgress.bind(this) });
-	    this.openSound2 = new Howl({ urls: ["sounds/open-bubble-3." + this.ext], rate: 0.15, onload: this.updateProgress.bind(this) });
-	    this.bounceSound = new Howl({ urls: ["sounds/bounce." + this.ext] });
-	    this.closeSound = new Howl({ urls: ["sounds/bubble-single-1." + this.ext], rate: 0.5, onload: this.updateProgress.bind(this) });
-	    this.closeSound2 = new Howl({ urls: ["sounds/bubble-single-1." + this.ext], rate: 0.75, onload: this.updateProgress.bind(this) });
-	    this.closeSound3 = new Howl({ urls: ["sounds/bubble-single-1." + this.ext], rate: 0.85, onload: this.updateProgress.bind(this) });
-	    this.closeScaleSound = new Howl({ urls: ["sounds/open-bubble-3." + this.ext], rate: 0.25, onload: this.updateProgress.bind(this) });
-	    this.closeBtnSound = new Howl({ urls: ["sounds/open-bubble-3." + this.ext], rate: 1, onload: this.updateProgress.bind(this) });
-	    this.loadImage("css/i/mojs-logo.png");
+	    this.openSound = new Howl({ urls: ["src/sounds/open-bubble-2." + this.ext], onload: this.updateProgress.bind(this) });
+	    this.openSound2 = new Howl({ urls: ["src/sounds/open-bubble-3." + this.ext], rate: 0.15, onload: this.updateProgress.bind(this) });
+	    this.bounceSound = new Howl({ urls: ["src/sounds/bounce." + this.ext] });
+	    this.closeSound = new Howl({ urls: ["src/sounds/bubble-single-1." + this.ext], rate: 0.5, onload: this.updateProgress.bind(this) });
+	    this.closeSound2 = new Howl({ urls: ["src/sounds/bubble-single-1." + this.ext], rate: 0.75, onload: this.updateProgress.bind(this) });
+	    this.closeSound3 = new Howl({ urls: ["src/sounds/bubble-single-1." + this.ext], rate: 0.85, onload: this.updateProgress.bind(this) });
+	    this.closeScaleSound = new Howl({ urls: ["src/sounds/open-bubble-3." + this.ext], rate: 0.25, onload: this.updateProgress.bind(this) });
+	    this.closeBtnSound = new Howl({ urls: ["src/sounds/open-bubble-3." + this.ext], rate: 1, onload: this.updateProgress.bind(this) });
+	    // this.loadImage("css/i/mojs-logo.png");
 	  },
 	  loadImage: function (url) {
 	    var image = new Image();
